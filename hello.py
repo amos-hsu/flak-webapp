0ee1a8a37f5a82ab1a85652f5b93bbd56425ca76
amos-hsu <751654494@qq.com>
2018-03-09 11:01:04 +0800

    Asynchronous emails

--- hello.py
+++ hello.py
@@ -57,12 +57,18 @@ class User(db.Model):
         return '<User %s>' % self.username
 
 
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 def send_mail(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_mail, args=[app, msg])
+    thr.start()
+    return thr
 
 
 class NameForm(FlaskForm):
